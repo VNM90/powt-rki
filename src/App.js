b527502a54dbb85f8b88607397bfa9746f740251
Arek Tkaczyk <artek547@gmail.com>
2018-08-20 16:23:46 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import SmartComponent from './SmartComponent'
-
+import Users from './Users'
 class App extends Component {
   render() {
     return (
@@ -11,11 +11,13 @@ class App extends Component {
             <Link to={'/hello'}> Hello </Link>
             <Link to={'/world'}> world </Link>
             <Link to={'/smart'}> smart </Link>
+            <Link to={'/users'}> Users </Link>
 
             <Route path={'/'} exact component={World} />
             <Route path={'/smart'} exact component={SmartComponent} />
             <Route path={'/hello'} exact component={Hello} />
             <Route path={'/world'} exact component={World} />
+            <Route path={'/users'} exact component={Users} />
           </div>
         </Router>
       </div>
